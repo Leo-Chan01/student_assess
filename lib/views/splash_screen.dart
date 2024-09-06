bd004c5e172ca665c56bb2c10209f857b9486ccf
Leo-Chan01 <ephraimleo16@gmail.com>
2024-09-06 08:54:38 +0100

    add splash screen UI

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,14 +1,68 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'package:go_router/go_router.dart';
+import 'package:student_assess/view_model/utils/config/routes.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+
+    _startRotationSequence();
+
+    Future.delayed(const Duration(seconds: 5), () {
+      if (mounted) {
+        context.go(AppRoutes.homeRoute);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Splash!"),
+        child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: _controller.value * 2 * pi,
+                child: child,
+              );
+            },
+            child: const FlutterLogo()),
       ),
     );
+  }
+
+  void _startRotationSequence() {
+    Future.doWhile(() async {
+      if (!mounted) return false;
+      await _controller.forward();
+      if (!mounted) return false;
+      await Future.delayed(const Duration(seconds: 1));
+      if (!mounted) return false;
+      _controller.reset();
+      return true;
+    });
   }
 }
