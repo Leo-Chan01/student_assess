fc3298878cc2fded6a3efc0613166192da19e925
Leo-Chan01 <ephraimleo16@gmail.com>
2024-09-08 14:17:53 +0100

    add student profile page

--- lib/views/student_profile/student_profile_page.dart
+++ lib/views/student_profile/student_profile_page.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:student_assess/view_model/utils/config/color.dart';
+import 'package:student_assess/view_model/utils/extension/num_extension.dart';
+import 'package:student_assess/views/widgets/student_assess_button_widget.dart';
 
 class StudentProfilePage extends StatelessWidget {
   const StudentProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Profile"),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 32.sp, horizontal: 18.sp),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Profile",
+                  style: 32.w700,
+                ),
+                SizedBox(height: 18.h),
+                ListTile(
+                  tileColor: AppColor.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.r)),
+                  title: Text("USERNAME", style: 18.w400),
+                  subtitle: Text("Your Username", style: 22.w600),
+                ),
+                SizedBox(height: 66.h),
+                StudentAssessButton(
+                    pressedAction: () {},
+                    buttonText: "Upload CGPA Data",
+                    buttonColor: AppColor.blue,
+                    buttonTextColor: AppColor.white),
+                SizedBox(height: 18.h),
+                StudentAssessButton(
+                    pressedAction: () {},
+                    buttonText: "Update Profile Information",
+                    buttonColor: AppColor.black,
+                    buttonTextColor: AppColor.white),
+                SizedBox(height: 18.h),
+                StudentAssessButton(
+                    pressedAction: () {},
+                    buttonText: "Delete All Data",
+                    buttonColor: Colors.red,
+                    buttonTextColor: AppColor.white)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
